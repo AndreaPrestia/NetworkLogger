fe1607809399ccd41023a0d4a56895ac78683205
Andrea Prestia <andrea.prestia@thetisit.com>
2022-11-29 11:58:23 +0100

    Added conflict in master

--- NetworkLogger.Core/LoggingMiddleware.cs
+++ NetworkLogger.Core/LoggingMiddleware.cs
@@ -32,6 +32,7 @@ public class LoggingMiddleware
 
         try
         {
+            Console.WriteLine("I'm a conflict in master");
             context.Request.EnableBuffering();
 
             requestBody = new StreamReader(context.Request.Body).ReadToEndAsync().Result;
